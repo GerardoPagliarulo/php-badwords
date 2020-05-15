4ba97d289c9f6dc5ddf20cba8c1d50f03a8ec4f1
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-05-15 17:47:17 +0200

    PHP - Badwords

--- index.php
+++ index.php
@@ -6,21 +6,21 @@
     <title>php-badwords</title>
 </head>
 <body>
-    
-    <?php
-    // Vars
-    $paragraph = '“Appartengo a un mondo senza argomenti. È forse per questo che, di volta in volta, ho litigato per poi riconciliarmi con esso. La sua mancanza di fondamento mi ha assuefatto a un’alternativa inesorabile tra partecipazione e disgusto”. Emil Cioran';
-    $string_length = strlen($paragraph);
-    $word = $_GET["word"];
-    $replace = str_replace($word, '***', $paragraph); 
-    //$replace = str_replace($_GET["name"], '***', $paragraph); 
-    ?>
-    <!-- Visualizzare paragrafo a schermo -->
-    <p><?php echo $paragraph ?></p>
-    <!-- Lunghezza paragrafo -->
-    <p>Lunghezza paragrafo: <?php echo $string_length ?></p>
-    <!-- Sostituzione della parola indesiderata -->
+
     <div>
+        <?php
+        // Vars
+        $paragraph = '“Appartengo a un mondo senza argomenti. È forse per questo che, di volta in volta, ho litigato per poi riconciliarmi con esso. La sua mancanza di fondamento mi ha assuefatto a un’alternativa inesorabile tra partecipazione e disgusto”. Emil Cioran';
+        $string_length = strlen($paragraph);
+        $word = $_GET["word"];
+        $replace = str_replace($word, '***', $paragraph); 
+        //$replace = str_replace($_GET["name"], '***', $paragraph); 
+        ?>
+        <!-- Visualizzare paragrafo a schermo -->
+        <p><?php echo $paragraph ?></p>
+        <!-- Lunghezza paragrafo -->
+        <p>Lunghezza paragrafo: <?php echo $string_length ?></p>
+        <!-- Sostituzione della parola indesiderata -->
         <strong>Sostituzione della parola indesiderata: </strong><?php echo $replace ?>
     </div>
 
